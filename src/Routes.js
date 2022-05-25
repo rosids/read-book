b927280989758183fddaa5b2fd2700bc86aea211
Rosiele David <rosieleserpa@hotmail.com>
2022-05-25 19:39:48 -0300

    add rota de detalhes do livro

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Provider from './context/Provider';
 import Home from './pages/Home';
+import Details from './pages/Details';
 
 function AllRoutes() {
   return (
@@ -8,6 +9,7 @@ function AllRoutes() {
       <Provider>
         <Routes>
           <Route exact path="/" element={ <Home /> } />
+          <Route exact path="/:id" element={ <Details />} />
         </Routes>
       </Provider>
     </BrowserRouter>
