f68bb24562d4fd768a91a357a9ea317c09218171
Rosiele David <rosieleserpa@hotmail.com>
2022-05-26 17:57:06 -0300

    corrige e add diálogo de remoção em card de detalhes do livro

--- src/components/BasicCard.js
+++ src/components/BasicCard.js
@@ -4,30 +4,38 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import DeleteDialog from './DeleteDialog';
 
 export default function BasicCard({ book }) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleDelete = () => {
+    setOpen(true);
+  }
+
   return (
-    <Card sx={{ minWidth: 275 }}>
-      <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Livro
-        </Typography>
-        {book.map(book => (
-          <div key={book._id}>
-            <Typography variant="h5" component="div">
-              {book.name}
-            </Typography>
-            <br />
-            <Typography variant="body">
-              {book.comment}
-            </Typography>
-          </div>
-        ))}
-      </CardContent>
-      <CardActions>
-        <Button size="small">Atualizar</Button>
-        <Button size="small" color="error">Excluir</Button>
-      </CardActions>
-    </Card>
+    <>
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            Livro
+          </Typography>
+          <Typography variant="h5" component="div">
+            {book.name}
+          </Typography>
+          <br />
+          <Typography variant="body">
+            {book.comment ? book.comment : 'Não há comentários sobre o livro.'}
+          </Typography>
+        </CardContent>
+        <CardActions style={{ justifyContent: 'flex-end' }}>
+          <Button size="small">Atualizar</Button>
+          <Button size="small" color="error" onClick={handleDelete}>Excluir</Button>
+        </CardActions>
+      </Card>
+      <div style={{display: 'none'}}>
+        <DeleteDialog open={open} setOpen={setOpen} bookId={book._id}  />
+      </div>
+    </>
   );
 }
